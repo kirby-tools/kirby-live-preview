57640d0a4adddd97accc2d6d602c0fe246985a2c
Johann Schopplich <mail@johannschopplich.com>
2024-06-17 15:31:53 +0200

    chore: update translations

--- src/extensions/translations.php
+++ src/extensions/translations.php
@@ -5,18 +5,18 @@ return [
         'johannschopplich.preview.label' => 'Live Preview',
         'johannschopplich.preview.error.render' => 'Preview failed',
         'johannschopplich.preview.license.buy' => 'Buy a license',
-        'johannschopplich.preview.license.activate' => 'Activate',
+        'johannschopplich.preview.license.activate' => 'Activate'
     ],
     'de' => [
         'johannschopplich.preview.label' => 'Live-Vorschau',
         'johannschopplich.preview.error.render' => 'Vorschau fehlgeschlagen',
         'johannschopplich.preview.license.buy' => 'Lizenz kaufen',
-        'johannschopplich.preview.license.activate' => 'Aktivieren',
+        'johannschopplich.preview.license.activate' => 'Aktivieren'
     ],
     'fr' => [
         'johannschopplich.preview.label' => 'Aperçu en direct',
         'johannschopplich.preview.error.render' => 'Aperçu échoué',
         'johannschopplich.preview.license.buy' => 'Acheter une licence',
-        'johannschopplich.preview.license.activate' => 'Activer',
+        'johannschopplich.preview.license.activate' => 'Activer'
     ]
 ];
