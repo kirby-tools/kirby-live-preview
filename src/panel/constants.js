403946baf9e2feb6cb9fd41195ae89263e4d1bd1
Johann Schopplich <mail@johannschopplich.com>
2024-12-20 14:53:12 +0100

    perf: use shared logger from `kirbyuse`

--- src/panel/constants.js
+++ src/panel/constants.js
@@ -1,4 +1,3 @@
 export const PLUGIN_CONTEXT_API_ROUTE = "__live-preview__/context";
 
 export const LOG_LEVELS = ["error", "warn", "info", "debug"];
-export const STORAGE_KEY_PREFIX = "kirby$preview$";
--- src/panel/utils/storage.js
+++
@@ -1,6 +0,0 @@
-import { hash } from "ohash";
-import { STORAGE_KEY_PREFIX } from "../constants";
-
-export function getHashedStorageKey(...args) {
-  return `${STORAGE_KEY_PREFIX}${hash([...args])}`;
-}
